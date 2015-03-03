956385c7c74eb7e326df0aae333a03949c242f2e
frankyueh <yueh.frank@gmail.com>
2015-03-04 07:43:09 +0800

    add files

---
+++ .autoindex/logout.php
@@ -0,0 +1,21 @@
+<?php require_once 'header.inc' ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+		<title>Re-authenticating...</title>
+		
+		<meta http-equiv="refresh" content="0;URL='<?= getRootUrl() ?>'" />
+		
+	</head>
+	<body>
+		<div class="container">
+			<div class="well">
+				<a href="<?= getRootUrl() ?>">Re-authenticating...</a>
+			</div>
+		</div>
+	</body>
+</html>
